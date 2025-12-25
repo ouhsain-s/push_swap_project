623b5206b149b04cd056a71883d56f24e4fe2ed4
Salahdine Ouhsain <souhsain@e1r7p3.1337.ma>
2025-12-25 13:17:26 +0100

    initial commit

---
+++ ft_atoi.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atio.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souhsain <souhsain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/25 12:49:01 by souhsain          #+#    #+#             */
+/*   Updated: 2025/12/25 12:49:45 by souhsain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static  int     ft_isspace(char c)
+{
+        if ((c >= 9 && c <= 13) || c == 32)
+                return (1);
+        return (0);
+}
+
+static  int     check_overflow(long *num, long tmp, int sign)
+{
+        if (*num / 10 != tmp)
+        {
+                if (sign == -1)
+                        *num = 0;
+                else
+                        *num = -1;
+                return (1);
+        }
+        return (0);
+}
+
+int     ft_atoi(const char *str)
+{
+        int             count;
+        int             sign;
+        long    num;
+        long    tmp;
+
+        num = 0;
+        sign = 1;
+        count = 0;
+        while (ft_isspace(str[count]))
+                count++;
+        if (str[count] == '-')
+                sign = -1;
+        if (str[count] == '+' || str[count] == '-')
+                count++;
+        while (str[count] != '\0' && ft_isdigit(str[count]))
+        {
+                tmp = num;
+                num = (num * 10) + (str[count] - '0');
+                if (check_overflow(&num, tmp, sign))
+                        return (num);
+                count++;
+        }
+        return (num * sign);
+}
---
+++ header.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souhsain <souhsain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/25 10:48:27 by souhsain          #+#    #+#             */
+/*   Updated: 2025/12/25 12:31:15 by souhsain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct st_stack
+{
+    char    *data;
+    int size;
+    int content;
+} t_stack;
+
+#endif
