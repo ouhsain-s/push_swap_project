17169fff901afb05a49d7567ef88093ce1b3f281
Salahdine Ouhsain <souhsain@e1r6p4.1337.ma>
2025-12-25 21:28:49 +0100

    add up

--- stack_manager.c
+++ stack_manager.c
@@ -6,7 +6,7 @@
 /*   By: souhsain <souhsain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/25 16:06:52 by souhsain          #+#    #+#             */
-/*   Updated: 2025/12/25 17:38:53 by souhsain         ###   ########.fr       */
+/*   Updated: 2025/12/25 21:25:11 by souhsain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,4 +46,59 @@ int realoc_stack(int size, t_stack stack)
     }
     free(old_arr);
     return(1);
+}
+
+int	is_complet_number(char *s, int j)
+{
+	if (j == 0)
+		return (0);
+	if (ft_isdigit(s[j - 1]) && !ft_isdigit(s[j]))
+		return (1);
+	if (s[j + 1] == '\0' && ft_isdigit(s[j]))
+		return(1);
+	return (0);
+}
+static int	counvert_num(char *s, t_stack stack, int *start, int j)
+{
+	stack.data[stack.content] = ft_atoi(ft_substr(s, *start, j - *start + 1));			
+	if (stack.content == stack.size - 1)
+	{
+		if (!realloc_size(500, stack));
+			return (0);
+	}
+	if ((stack.data[stack.content] == -1) && !search_Minus_one(*start, j - *start + 1))
+	//create function that search -1 in string provided to it 
+	
+		return (0);
+	stack.content++;
+	*start  = j + 1;
+	return (1);
+}
+int	add_numbers_tostack(t_stack stack, int num_s, char ** arr_s)
+{
+	int		i;
+	int		j;
+	int		start;
+
+	if (!allocate_new_stack(stack, 1000))
+		return(0);
+	i = 1;
+	while (i < num_s)
+	{
+		j = 0;
+		start = 0;
+		while (arr_s[i][j] != '\0')
+		{
+			if (!is_recognized_char(arr_s[i], j))
+				return(0);
+			if (is_complet_number(arr_s[i], j))
+			{
+				if (!counvert_num(arr_s[i], stack, &start, j))
+					return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
